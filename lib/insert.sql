da69fcbabac1d984b9c2c2ac9c58297dfb5c0f44
christian williams <christian.williamsj@gmail.com>
2019-11-19 03:03:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears VALUES (4,"Sargeant Brown", 15, "M", "brown", "hungry", 1);
 INSERT INTO bears VALUES (5,"Melissa", 7, "F", "brown", "lazy", 1);
 INSERT INTO bears VALUES (6,"Grinch", 3, "M", "Black", "aggressive", 1);
 INSERT INTO bears VALUES (7,"Wendy", 13, "F", "white", "lazy", 0);
-INSERT INTO bears VALUES (8,"NULL", 55, "M", "black", "aggressive", 0);
+INSERT INTO bears VALUES (8,NULL, 55, "M", "black", "aggressive", 0);
